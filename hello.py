9bbe93145804c9216f6c4bdaaa7f767d72d692cf
t2wole <t2wole@gmail.com>
2022-04-15 13:38:09 +0900

    db

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
@@ -20,4 +24,9 @@ for tr in trs:
         rank = tr.select_one('td:nth-child(1) > img')['alt']
         title = a_tag.text
         star = tr.select_one('td.point').text
-        print(rank, title, star)
+        doc = {
+            'rank': rank,
+            'title': title,
+            'star': star
+        }
+        db.movies.insert_one(doc)
