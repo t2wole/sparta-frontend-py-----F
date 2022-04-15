44626e38b468bae71518918aa146e995ab8f2e26
t2wole <t2wole@gmail.com>
2022-04-15 13:51:43 +0900

    find 9.39

---
+++ dbquiz.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+movie = db.movies.find_one({'title': '매트릭스'})
+
+target_star = movie['star']
+target_movies = list(db.movies.find({'star':target_star},{'_id':False}))
+
+for target in target_movies:
+    print(target['title'])
